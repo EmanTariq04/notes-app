bf9b809bfccdca1137b81f5bcd1a300d63741565
Eman Tariq <emantariq.1004@gmail.com>
2025-07-21 22:54:32 +0500

    commit

--- scrpit.js
+++ scrpit.js
@@ -2,6 +2,10 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML)
+}
+
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -9,4 +13,11 @@ createBtn.addEventListener("click", () => {
     inputBox.setAttribute("contenteditable", "true");
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img)
+})
+
+notesContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
 })
